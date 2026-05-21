ef8d3dd00e520c02816eae00269768bc98f00723
botinho12 <botivinicios77@gmail.com>
2026-05-21 20:21:54 -0300

    adicionando link direto pelo cards dos contatos

--- Database/Context.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace ProfileMVC.Database;
-
-public class Context : DbContext
-{
-    public Context(DbContextOptions<Context> options) :  base(options){}
-    
-}
